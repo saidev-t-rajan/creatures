84b961da6598270dc630156a5e9d95ece514f710
saidev-t-rajan <saidevrajan@gmail.com>
2016-02-11 00:36:45 +1100

    changes

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('<script src="synaptic.js"></script><script src="creature.js"></script><script src="world.js"></script><h1>Hello World!</h1><div id="canvas-container" class="col-md-6"><canvas id="canvas" width="555" height="550"></canvas></div>');
-});
+app.use('/synaptic', express.static(__dirname + '/node_modules/synaptic/dist/'));
+app.use(express.static('public'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
